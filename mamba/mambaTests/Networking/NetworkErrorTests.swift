a9eb61a24fa229ffa59d89b82a58b647990ea275
armandkamffer <akamffer@mweb.co.za>
2020-08-10 13:28:27 +0200

    MAM-41: Add NetworkError tests

--- mamba/mambaTests/Networking/NetworkErrorTests.swift
+++ mamba/mambaTests/Networking/NetworkErrorTests.swift
@@ -11,24 +11,137 @@ import XCTest
 
 class NetworkErrorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testDecodingTypeMismatchErrorCode() throws {
+        // Given: a mock error description
+        let mockErrorDescription = "Unit tests error"
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.decodingTypeMistmatch(mockErrorDescription)
+        
+        // Then: the error code matches expected value
+        XCTAssertEqual(error.errorCode, "3100")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testDecodingTypeMismatchErrorDescription() throws {
+        // Given: a mock error description
+        let mockErrorDescription = "Unit tests error"
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.decodingTypeMistmatch(mockErrorDescription)
+        
+        // Then: the error description matches expected value
+        XCTAssertEqual(error.errorDescription, mockErrorDescription)
+    }
+    
+    func testDecodingValueNotFoundErrorCode() throws {
+        // Given: a mock error description
+        let mockErrorDescription = "Unit tests error"
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.decodingValueNotFound(mockErrorDescription)
+        
+        // Then: the error code matches expected value
+        XCTAssertEqual(error.errorCode, "3101")
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDecodingValueNotFoundErrorDescription() throws {
+        // Given: a mock error description
+        let mockErrorDescription = "Unit tests error"
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.decodingValueNotFound(mockErrorDescription)
+        
+        // Then: the error description matches expected value
+        XCTAssertEqual(error.errorDescription, mockErrorDescription)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testDecodingKeyNotFoundErrorCode() throws {
+        // Given: a mock error description
+        let mockErrorDescription = "Unit tests error"
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.decodingKeyNotFound(mockErrorDescription)
+        
+        // Then: the error code matches expected value
+        XCTAssertEqual(error.errorCode, "3102")
     }
 
+    func testDecodingKeyNotFoundErrorDescription() throws {
+        // Given: a mock error description
+        let mockErrorDescription = "Unit tests error"
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.decodingKeyNotFound(mockErrorDescription)
+        
+        // Then: the error description matches expected value
+        XCTAssertEqual(error.errorDescription, mockErrorDescription)
+    }
+    
+    func testDecodingDataCorruptedErrorCode() throws {
+        // Given: a mock error description
+        let mockErrorDescription = "Unit tests error"
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.decodingDataCorrupted(mockErrorDescription)
+        
+        // Then: the error code matches expected value
+        XCTAssertEqual(error.errorCode, "3103")
+    }
+
+    func testDecodingDataCorruptedErrorDescription() throws {
+        // Given: a mock error description
+        let mockErrorDescription = "Unit tests error"
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.decodingDataCorrupted(mockErrorDescription)
+        
+        // Then: the error description matches expected value
+        XCTAssertEqual(error.errorDescription, mockErrorDescription)
+    }
+    
+    func testUnknownDecodingErrorCode() throws {
+        // Given: a mock decoding error
+        let mockDecodingError = DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "Test error description"))
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.unknownDecodingError(mockDecodingError)
+        
+        // Then: the error code matches expected value
+        XCTAssertEqual(error.errorCode, "3104")
+    }
+
+    func testUnknownDecodingErrorDescription() throws {
+        // Given: a mock decoding error
+        let mockDecodingError = DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "Test error description"))
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.unknownDecodingError(mockDecodingError)
+        
+        // Then: the error description matches expected value
+        XCTAssertEqual(error.errorDescription, mockDecodingError.localizedDescription)
+    }
+    
+    func testUnknownErrorCode() throws {
+        // Given: a mock error
+        let mockError = NSError(domain: "", code: 0, userInfo: nil)
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.unknownError(mockError)
+        
+        // Then: the error code matches expected value
+        XCTAssertEqual(error.errorCode, "3105")
+    }
+
+    func testUnknownErrorDescription() throws {
+        // Given: a mock error
+        let mockError = NSError(domain: "", code: 0, userInfo: nil)
+        
+        // When: NetworkError is mapped
+        let error = NetworkError.unknownError(mockError)
+        
+        // Then: the error description matches expected value
+        XCTAssertEqual(error.errorDescription, "The operation couldn’t be completed. ( error 0.)")
+    }
+    
 }

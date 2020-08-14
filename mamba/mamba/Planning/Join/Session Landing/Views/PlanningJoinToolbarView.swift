e65055686d74f723149d3d29612e5c322246b0a4
Armand Kamffer <armand.kamffer@entelect.co.za>
2020-08-14 10:10:09 +0200

    MAM-61: Add basic context menu for host participants list

---
+++ mamba/mamba/Planning/Join/Session Landing/Views/PlanningJoinToolbarView.swift
@@ -0,0 +1,21 @@
+//
+//  PlanningJoinToolbarView.swift
+//  mamba
+//
+//  Created by Armand Kamffer on 2020/08/14.
+//  Copyright © 2020 Armand Kamffer. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlanningJoinToolbarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlanningJoinToolbarView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanningJoinToolbarView()
+    }
+}
---
+++ mamba/mamba/Styling/Components/Button/SystemImageTextButton.swift
@@ -0,0 +1,21 @@
+//
+//  SystemImageTextButton.swift
+//  mamba
+//
+//  Created by Armand Kamffer on 2020/08/14.
+//  Copyright © 2020 Armand Kamffer. All rights reserved.
+//
+
+import SwiftUI
+
+struct SystemImageTextButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SystemImageTextButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SystemImageTextButton()
+    }
+}

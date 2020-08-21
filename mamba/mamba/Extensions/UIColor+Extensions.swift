40d9c572ee1bf0bb1b117f7400de151e5e4bc56b
Armand Kamffer <armand.kamffer@entelect.co.za>
2020-08-21 15:07:22 +0200

    MAM-73: Remove commented code

--- mamba/mamba/Extensions/UIColor+Extensions.swift
+++ mamba/mamba/Extensions/UIColor+Extensions.swift
@@ -17,28 +17,6 @@ extension UIColor {
         return self.adjust(by: -abs(percentage))
     }
     
-//    func adjust(by percentage: CGFloat) -> UIColor {
-//        var alpha, hue, saturation, brightness, red, green, blue, white : CGFloat
-//        (alpha, hue, saturation, brightness, red, green, blue, white) = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-//
-//        let multiplier = percentage / 100.0
-//
-//        if self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
-//            let newBrightness: CGFloat = max(min(brightness + multiplier*brightness, 1.0), 0.0)
-//            return UIColor(hue: hue, saturation: saturation, brightness: newBrightness, alpha: alpha)
-//        } else if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
-//            let newRed: CGFloat = min(max(red + multiplier*red, 0.0), 1.0)
-//            let newGreen: CGFloat = min(max(green + multiplier*green, 0.0), 1.0)
-//            let newBlue: CGFloat = min(max(blue + multiplier*blue, 0.0), 1.0)
-//            return UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: alpha)
-//        } else if self.getWhite(&white, alpha: &alpha) {
-//            let newWhite: CGFloat = (white + multiplier*white)
-//            return UIColor(white: newWhite, alpha: alpha)
-//        }
-//
-//        return self
-//    }
-    
     func adjust(by percentage: CGFloat = 30.0) -> UIColor {
         var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
         if self.getHue(&h, saturation: &s, brightness: &b, alpha: &a) {
